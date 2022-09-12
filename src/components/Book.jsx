2ece29cbf5f56d16700fba3c0660c15cf2cd8b93
Noi Sinnang <noisinnang@nois-mbp.mynetworksettings.com>
2022-09-11 21:08:00 -0400

    destructue data into seperate file

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import '../style/index.css'
 
-const Book = (props) => {
-  const {img, title, author} = props
+const Book = ({img, title, author}) => {
+  const  clickHandler = () => {
+
+  }
   return (
     <article className='book'>
       <img className='book-image' src={img} alt={title} />
       <h1 className='book-title'> {title} </h1>
       <p className='book-author'>{author}</p>
+      <button type="button onClick={() => clickHandler()}">reference example</button>
     </article>
   )
 }
---
+++ src/components/booksData.js
@@ -0,0 +1,20 @@
+export const books = [ 
+  {
+  "id": 1,
+  "img": "https://m.media-amazon.com/images/I/91HHxxtA1wL._AC_UL640_FMwebp_QL65_.jpg",
+  "title": "The Wonderful Things You Will Be",
+  "author": "Emily Winfield Martin"
+  },
+  {
+    "id": 2,
+    "img": "https://m.media-amazon.com/images/I/81X3ijILdJL._AC_UL640_FMwebp_QL65_.jpg",
+    "title": "How to Catch a Mermaid",
+    "author": "Adam Wallace and Andy Elkerton"
+    },
+    {
+      "id": 3,
+      "img": "https://m.media-amazon.com/images/I/812oNxmfS0L._AC_UL640_FMwebp_QL65_.jpg",
+      "title": "The Good Egg",
+      "author": "Jory John and Pete Oswald"
+      }
+]
